4e19a97ccd1860af3b97e1900a4e71faed5b6e4b
Veronica <VMGranite@gmail.com>
2023-09-14 07:10:09 -0400

    fix: include additional partitions with quadrants 1,2,3 & 4 of x y grid

--- db/database-seed.sql
+++ db/database-seed.sql
@@ -6,20 +6,8 @@ CREATE TABLE unique_areas_cleaned (
     duration DECIMAL,
     CONSTRAINT unique_areas_cleaned_pkey PRIMARY KEY(id)
 );
-
--- Grid Range x: -10 to -1 and y: -10 to -1
-CREATE TABLE grid_coordinates_partition1 (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    x INT,
-    y INT,
-    is_cleaned BOOLEAN DEFAULT false,
-    CHECK (x >= -10 AND x <= -1 AND y >= -10 AND y <= -1) 
-);
-
 -- Grid Range x: 0 to 10 and y: 0 to 10
-CREATE TABLE grid_coordinates_partition2 (
+CREATE TABLE grid_coordinates_partition1 (
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -29,26 +17,79 @@ CREATE TABLE grid_coordinates_partition2 (
     CHECK (x >= 0 AND x <= 10 AND y >= 0 AND y <= 10) 
 );
 
--- Populate the first partition (-10 to -1)
+-- Grid Range x: 0 to 10 and y: -10 to -1
+CREATE TABLE grid_coordinates_partition2 (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    x INT,
+    y INT,
+    is_cleaned BOOLEAN DEFAULT false,
+    CHECK (x >= 0 AND x <= 10 AND y >= -10 AND y <= -1) 
+);
+
+-- Grid Range x: -10 to -1 and y: -10 to -1
+CREATE TABLE grid_coordinates_partition3 (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    x INT,
+    y INT,
+    is_cleaned BOOLEAN DEFAULT false,
+    CHECK (x >= -10 AND x <= -1 AND y >= -10 AND y <= -1) 
+);
+
+-- Grid Range x: -10 to -1 and y: 0 to 10
+CREATE TABLE grid_coordinates_partition4 (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    x INT,
+    y INT,
+    is_cleaned BOOLEAN DEFAULT false,
+    CHECK (x >= -10 AND x <= -1 AND y >= 0 AND y <= 10) 
+);
+
+-- Populate  partition 1 with Quadrant 1 X Y Grid Coordinates
 DO $$ 
 BEGIN
-    FOR x_val IN -10..-1 LOOP
-        FOR y_val IN -10..-1 LOOP
+    FOR x_val IN 0..10 LOOP
+        FOR y_val IN 0..10 LOOP
             INSERT INTO grid_coordinates_partition1(x,y) VALUES (x_val, y_val);
         END LOOP;
     END LOOP;
     COMMIT;
 END $$;
 
--- Populate the second partition (0 to 10)
+-- Populate  partition 2 with Quadrant 2 X Y Grid Coordinates
 DO $$ 
 BEGIN
     FOR x_val IN 0..10 LOOP
-        FOR y_val IN 0..10 LOOP
+        FOR y_val IN -10..-1 LOOP
             INSERT INTO grid_coordinates_partition2(x,y) VALUES (x_val, y_val);
         END LOOP;
     END LOOP;
     COMMIT;
 END $$;
 
+-- Populate partition 3 with Quadrant 3 X Y Grid Coordinates
+DO $$ 
+BEGIN
+    FOR x_val IN -10..-1 LOOP
+        FOR y_val IN -10..-1 LOOP
+            INSERT INTO grid_coordinates_partition3(x,y) VALUES (x_val, y_val);
+        END LOOP;
+    END LOOP;
+    COMMIT;
+END $$;
 
+-- Populate partition 4 with Quadrant 4 X Y Grid Coordinates
+DO $$ 
+BEGIN
+    FOR x_val IN -10..-1 LOOP
+        FOR y_val IN 0..10 LOOP
+            INSERT INTO grid_coordinates_partition4(x,y) VALUES (x_val, y_val);
+        END LOOP;
+    END LOOP;
+    COMMIT;
+END $$;
--- src/databaseFunctions.ts
+++ src/databaseFunctions.ts
@@ -2,17 +2,22 @@ import { pool } from './app';
 
 export async function isAreaCleaned(input_x: number, input_y: number): Promise<boolean> {
     try {
-        const data = await pool.query(`SELECT is_cleaned FROM grid_coordinates WHERE x = $1 AND y = $2`, [input_x, input_y]);
+        const partition = checkDesignatedPartition(input_x, input_y)
+        const partition_name = 'grid_coordinates_partition' + partition
+        const data = await pool.query(`SELECT is_cleaned FROM ${partition_name} WHERE x = $1 AND y = $2`, [input_x, input_y]);
         return data.rows[0].is_cleaned;
+
     } catch (error) {
         console.error('Error fetching data:', error);
         throw error;
-    } 
+    }
 }
 
 export async function updateAreaToClean(input_x: number, input_y: number): Promise<void> {
     try {
-        await pool.query(`UPDATE grid_coordinates SET is_cleaned = TRUE WHERE x = $1 AND y = $2`, [input_x, input_y]);
+        const partition = checkDesignatedPartition(input_x, input_y)
+        const partition_name = 'grid_coordinates_partition' + partition
+        await pool.query(`UPDATE ${partition_name} SET is_cleaned = TRUE WHERE x = $1 AND y = $2`, [input_x, input_y]);
         console.log('is_cleaned set to true.');
     } catch (error) {
         console.error('Error updating data:', error);
@@ -22,7 +27,7 @@ export async function updateAreaToClean(input_x: number, input_y: number): Promi
 
 export async function makeFloorDirty(): Promise<void> {
     try {
-        await pool.query('UPDATE grid_coordinates SET is_cleaned = FALSE');
+        await pool.query('UPDATE grid_coordinates_partition1 SET is_cleaned = FALSE; UPDATE grid_coordinates_partition2 SET is_cleaned = FALSE; UPDATE grid_coordinates_partition3 SET is_cleaned = FALSE; UPDATE grid_coordinates_partition4 SET is_cleaned = FALSE;');
         console.log('The Floor is dirty!');
     } catch (error) {
         console.error('Error updating data:', error);
@@ -55,5 +60,20 @@ export async function getLastTripEntryIdAndTimeStamp(): Promise<{ id: string; ti
     } catch (error) {
         console.error('Error fetching data:', error);
         throw error;
+    }
+}
+
+function checkDesignatedPartition(input_x: number, input_y: number): number {
+    if (input_x >= 0 && input_y >= 0) {
+        return 1
+    } else if (input_x >= 0 && input_y <= -1) {
+        return 2
+    } else if (input_x <= -1 && input_y <= -1) {
+        return 3
+    } else if (input_x <= -1 && input_y >= -0) {
+        return 4
+    } else {
+        console.log('This is an invalid partition option.');
+        return 0
     }
 }
