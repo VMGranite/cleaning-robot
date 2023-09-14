71e5e7aac45af6db3841457966d0408cc5003c9b
Veronica <VMGranite@gmail.com>
2023-09-14 01:06:21 -0400

    working: create two partitions

--- db/database-seed.sql
+++ db/database-seed.sql
@@ -7,33 +7,48 @@ CREATE TABLE unique_areas_cleaned (
     CONSTRAINT unique_areas_cleaned_pkey PRIMARY KEY(id)
 );
 
-CREATE TABLE grid_coordinates (
-    id SERIAL,
+-- Grid Range x: -10 to -1 and y: -10 to -1
+CREATE TABLE grid_coordinates_partition1 (
+    id SERIAL PRIMARY KEY,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     x INT,
     y INT,
     is_cleaned BOOLEAN DEFAULT false,
-    CONSTRAINT grid_coordinates_pkey PRIMARY KEY(id)
+    CHECK (x >= -10 AND x <= -1 AND y >= -10 AND y <= -1) 
 );
 
--- Create a trigger function to update the updated_at column on UPDATE
-CREATE OR REPLACE FUNCTION update_updated_at()
-RETURNS TRIGGER AS $$
+-- Grid Range x: 0 to 10 and y: 0 to 10
+CREATE TABLE grid_coordinates_partition2 (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    x INT,
+    y INT,
+    is_cleaned BOOLEAN DEFAULT false,
+    CHECK (x >= 0 AND x <= 10 AND y >= 0 AND y <= 10) 
+);
+
+-- Populate the first partition (-10 to -1)
+DO $$ 
 BEGIN
-    NEW.updated_at = CURRENT_TIMESTAMP;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+    FOR x_val IN -10..-1 LOOP
+        FOR y_val IN -10..-1 LOOP
+            INSERT INTO grid_coordinates_partition1(x,y) VALUES (x_val, y_val);
+        END LOOP;
+    END LOOP;
+    COMMIT;
+END $$;
 
--- Create a trigger that calls the update_updated_at function after UPDATE
-CREATE TRIGGER grid_coordinates_update_trigger
-BEFORE UPDATE ON grid_coordinates
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at();
+-- Populate the second partition (0 to 10)
+DO $$ 
+BEGIN
+    FOR x_val IN 0..10 LOOP
+        FOR y_val IN 0..10 LOOP
+            INSERT INTO grid_coordinates_partition2(x,y) VALUES (x_val, y_val);
+        END LOOP;
+    END LOOP;
+    COMMIT;
+END $$;
 
--- Populate an X,Y grid from -10 to 10
-INSERT INTO grid_coordinates (x, y)
-SELECT x, y
-FROM generate_series(-10, 10) AS x
-CROSS JOIN generate_series(-10, 10) AS y;
+
